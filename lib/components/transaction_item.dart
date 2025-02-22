40ab4e3edce1a3216c8ef93615baee8904291e8a
Cleiton Araujo <cleitonamc@gmail.com>
2025-02-22 20:55:51 -0300

    componente transaction_item

---
+++ lib/components/transaction_item.dart
@@ -0,0 +1,75 @@
+
+
+import 'package:espenses/models/transactions.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class TransactionItem extends StatelessWidget {
+  final Transaction tr;
+  final void Function(String p1) onRemove;
+
+  const TransactionItem({
+    super.key,
+    required this.tr,
+    required this.onRemove,
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: const EdgeInsets.symmetric(
+        vertical: 8,
+        horizontal: 5,
+      ),
+      child: ListTile(
+        leading: Container(
+          //antes era o CircleAvatar com backgroundColor: e radius: 30
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: Theme.of(context).colorScheme.primary,
+          ),
+    
+          child: Padding(
+            padding: const EdgeInsets.all(11),
+            child: FittedBox(
+              child: Text(
+                'R\$${tr.value.toStringAsFixed(2)}',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ),
+        ),
+    
+        //Titulo da transação
+        title: Text(
+          tr.title,
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+    
+        //Subtitulo com data
+        subtitle: Text(
+          DateFormat('d / MM / y').format(tr.date),
+        ),
+    
+        //icone para remoção com checagem de largura de tela
+        trailing: MediaQuery.of(context).size.width > 480
+            ? TextButton.icon(
+                onPressed: () => onRemove(tr.id),
+                label: const Text('Excluir'),
+                icon: const Icon(Icons.delete),
+              )
+            : IconButton(
+                onPressed: () => onRemove(tr.id),
+                icon: const Icon(Icons.delete),
+                color: Theme.of(context).colorScheme.error,
+              ),
+      ),
+    );
+  }
+}
--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -1,8 +1,9 @@
 // ignore_for_file: use_key_in_widget_constructors, sized_box_for_whitespace, prefer_const_constructors, prefer_const_constructors_in_immutables
 
+import 'package:espenses/components/transaction_item.dart';
 import 'package:flutter/material.dart';
 import '../models/transactions.dart';
-import 'package:intl/intl.dart';
+
 
 class TransactionList extends StatelessWidget {
   // const TransactionList({super.key});
@@ -45,60 +46,7 @@ class TransactionList extends StatelessWidget {
               final tr = transactions[index]; //recebe os dados da transação
 
               //retorna um card
-              return Card(
-                elevation: 6,
-                margin: EdgeInsets.symmetric(
-                  vertical: 8,
-                  horizontal: 5,
-                ),
-                child: ListTile(
-                  leading: Container(
-                    //antes era o CircleAvatar com backgroundColor: e radius: 30
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      color: Theme.of(context).colorScheme.primary,
-                    ),
-
-                    child: Padding(
-                      padding: const EdgeInsets.all(11),
-                      child: FittedBox(
-                        child: Text(
-                          'R\$${tr.value.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            color: Theme.of(context).colorScheme.onPrimary,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-
-                  //Titulo da transação
-                  title: Text(
-                    tr.title,
-                    style: Theme.of(context).textTheme.titleMedium,
-                  ),
-
-                  //Subtitulo com data
-                  subtitle: Text(
-                    DateFormat('d / MM / y').format(tr.date),
-                  ),
-
-                  //icone para remoção com checagem de largura de tela
-                  trailing: MediaQuery.of(context).size.width > 480
-                      ? TextButton.icon(
-                          onPressed: () => onRemove(tr.id),
-                          label: Text('Excluir'),
-                          icon: Icon(Icons.delete),
-                        )
-                      : IconButton(
-                          onPressed: () => onRemove(tr.id),
-                          icon: Icon(Icons.delete),
-                          color: Theme.of(context).colorScheme.error,
-                        ),
-                ),
-              );
+              return TransactionItem(tr: tr, onRemove: onRemove);
             },
             // children: transactions.map((tr) {
             //   // cada transação na lista é um card
@@ -107,3 +55,5 @@ class TransactionList extends StatelessWidget {
           );
   }
 }
+
+
